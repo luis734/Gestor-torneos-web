a0a298518fe29a9bab0b101fcf01b5f39ecb02dc
Luis Villa <luis.villa@111btec.com>
2026-06-01 15:19:02 -0600

    (TEST) Se añadieron pruebas unitarias para la función validateTournamentName.
    - Se creó un nuevo archivo de pruebas para validar la función `validateTournamentName`.
    - Se implementaron casos de prueba para verificar nombres de torneos válidos, nombres vacíos, nombres con solo espacios y nombres que exceden la longitud máxima permitida.

---
+++ src/features/tournament/validators/tournament/validateTournamentName.test.ts
@@ -0,0 +1,37 @@
+import { describe, expect, it } from "vitest";
+import { validateTournamentName } from "./validateTournamentName";
+
+describe("validateTournamentName", () => {
+    it("returns valida for a correct tournament name", () => {
+        const name = "Torneo pitero";
+
+        const result = validateTournamentName(name);
+
+        expect(result.isValid).toBe(true);
+        expect(result.errors).toEqual([]);
+    });
+
+    it("returns error when name is empty", () => {
+        const name = "";
+
+        const result = validateTournamentName(name);
+
+        expect(result.errors).toEqual(["Name is required"]);
+    });
+
+    it("returns error when name contains only spaces", () => {
+        const name = "     ";
+
+        const result = validateTournamentName(name);
+
+        expect(result.errors).toEqual(["Name is required"]);
+    });
+
+    it("returns error when name exceeds maximum length", () => {
+        const name = "a".repeat(31);
+
+        const result = validateTournamentName(name);
+
+        expect(result.errors).toEqual(["Name is too long (<30)"]);
+    });
+});
