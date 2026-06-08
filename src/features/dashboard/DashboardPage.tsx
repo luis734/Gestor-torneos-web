dae9c16d753e45def6c76bcedad5b41160d63aae
Luis Villa <luis.villa@111btec.com>
2026-06-08 13:59:04 -0600

    (FEATURE) Se limpio boilerplate, y se creo la estructura de tokens para los colores y estilos básicos.
    - Se definio el src/theme/index como archivo de estilos principal.
    - Se craron los archivos dark y ligth como variantes para temas principales (falta implementar el guardado y cambio de tema).
    - Se creo un archivo base de la pagina principal DashboardPage.
    - Se configuro vite.

---
+++ src/features/dashboard/DashboardPage.tsx
@@ -0,0 +1,9 @@
+export function DashboardPage() {
+    return (
+        <div className="bg-background p-8">
+            <h1 className="text-4x1 font-bold text-text-primary">
+                Tournament Dashboard
+            </h1>
+        </div>
+    );
+}
