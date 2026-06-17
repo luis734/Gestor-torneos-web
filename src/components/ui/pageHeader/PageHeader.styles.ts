5cbde6d1c9ed92496e807d74a86548c6922bef47
Luis Villa <luis.villa@111btec.com>
2026-06-17 14:22:35 -0600

    (FEATURE) Se creo el componente de header.
    - Se creo el componente `PageHeader`.
    - Se crearon los estilos y tipado para el componente generico de header.
    - Se ajusto el relleno del icono Trophy para mejora la visualizacion.

---
+++ src/components/ui/pageHeader/PageHeader.styles.ts
@@ -0,0 +1,7 @@
+export const PageStyles = {
+    container: "w-full bg-surface text-foreground border-b-border flex items-center gap-2 px-6 ease-in-out",
+    isSticky: "fixed top-0 left-0 z-50 py-2 text-body-small border-b shadow-sm",
+    noSticky: "relative py-4 text-h3 border-b-2",
+    icon: "flex items-center justify-center bg-brand rounded h-7 w-7",
+    transitions: "transition-all duration-300"
+}
---
+++ src/components/ui/pageHeader/PageHeader.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useRef, useState } from "react";
+import { TrophyIcon } from "../../../assets/icons";
+import { PageStyles } from "./PageHeader.styles";
+import { type PageProps } from "./PageHeader.types";
+
+export function PageHeader({pageTitle}:PageProps) {
+    const [isSticky, setIsSticky] = useState(false);
+    const headerRef = useRef<HTMLElement>(null);
+    const [headerHeight, setHeaderHeight] = useState(0);
+
+    useEffect(() => {
+        // Guardamos la altura original del header
+        if (headerRef.current) {
+            setHeaderHeight(headerRef.current.offsetHeight)
+        }
+
+        function handleScroll() {
+            setIsSticky(window.scrollY > headerHeight);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        handleScroll();
+
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, [headerHeight]);
+
+    return (
+        <>
+          {/* Placeholder invisible que mantiene el espacio cuando el header es fixed */}
+          {isSticky && <div style={{ height: headerHeight }} />}
+    
+          <header
+            ref={headerRef}
+            className={`${ PageStyles.container } ${PageStyles.transitions} ${ isSticky ? PageStyles.isSticky : PageStyles.noSticky }
+            `}
+          >
+            <div className={`${PageStyles.icon} ${isSticky ? "h-5 w-5" : "h-7 w-7"}`}>
+                <TrophyIcon className={`${PageStyles.transitions} ${isSticky ? "h-3 w-3" : "h-4 w-4"}`}/>
+            </div>
+            <span className={PageStyles.transitions}>
+                {pageTitle}
+            </span>
+          </header>
+        </>
+      );
+}
